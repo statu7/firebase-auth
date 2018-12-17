68ee2e1e7c70c2183ce31040433beb9cb340d006
U-ADOLB\b16155 <b16155@P27056.adolb.olb-int.de>
2018-12-17 16:43:25 +0100

    Sign Out with React und Firebase & Session Handling with Firebase/React.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
+import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes'
 
-const Navigation = ()=>(
+const Navigation = ({authUser}) => (
+    <div>{authUser ? <NavigationAuth/> : <NavigationNonAuth/>}</div>
+);
+
+
+const NavigationAuth = () => (
     <div>
         <ul>
-            <li>
-                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-            </li>
             <li>
                 <Link to={ROUTES.LANDING}>Landing</Link>
             </li>
@@ -19,11 +22,22 @@ const Navigation = ()=>(
                 <Link to={ROUTES.ACCOUNT}>Accoung</Link>
             </li>
             <li>
-                <Link to={ROUTES.ADMIN}>Admin</Link>
+                <SignOutButton/>
             </li>
-
         </ul>
     </div>
+);
+
+const NavigationNonAuth = () => (
+    <ul>
+        <li>
+            <Link to={ROUTES.LANDING}>Landing</Link>
+        </li>
+        <li>
+            <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+        </li>
+    </ul>
+
 );
 
 export default Navigation;
